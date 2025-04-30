ae8173513ac7cc4ad252bebeb872fa4807c0a615
seonwee <seonwee@qq.com>
2025-04-30 12:28:40 +0800

    fix bug

--- src/bump.h
+++ src/bump.h
@@ -10,7 +10,7 @@ void kissat_update_scores (struct kissat *);
 void kissat_rescale_scores (struct kissat *);
 void kissat_bump_variable (struct kissat *, unsigned idx);
 void kissat_bump_score_increment (struct kissat *);
-
+void kissat_bump_variables (struct kissat *);
 //CHB
 void kissat_bump_chb (struct kissat *, unsigned idx, double multiplier);
 void kissat_decay_chb (struct kissat *);
